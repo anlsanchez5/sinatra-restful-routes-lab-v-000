d900f4c0855e5df9db00956b0c9659f073a18323
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-04-05 22:41:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,8 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes/new' do
     recipe = Recipe.new(:name=> params[:name], :ingredients=> params[:ingredients], :cook_time=> params[:cook_time])
+    recipe.save
+
   end
 
 end
